6b3b98fd0eb89604a2637d77f687348cdcd2c22b
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-22 11:17:26 +0500

    sad

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import Slider from '../components/Slider/Slider'
 import "./Home.scss"
 import Categories from '../components/Categories/Categories'
+// import Card from '../components/Card/Card'
 // import FeaturedProducts from '../components/FeaturedProducts/FeaturedProducts'
 const Home = () => {
   return (
     <div className='home'>
       <Slider/>
       <Categories/>
+      {/* <Card/> */}
       {/* <FeaturedProducts type="featured"/>
       <FeaturedProducts type="trending"/> */}
     </div>
-  )
-}
+  );
+};
 
 export default Home
