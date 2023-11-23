c930340e141107732b2bd3609cf0ce834942a6de
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-23 10:56:53 +0500

    dkjf

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -33,31 +33,12 @@
 
 // export default FeaturedProducts;
 
-
 import React from 'react'
-import Card from '../Card/Card'
+import "./FeaturedProducts.scss"
 const FeaturedProducts = () => {
   return (
     <div>
-      <div className="featuredProducts">
-       <div className="top">
-         <h1>{type} products</h1>
-         <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-           eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
-           suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
-           lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
-           suspendisse ultrices gravida. Risus commodo viverra maecenas.
-         </p>
-       </div>
-       <div className="bottom">
-         {error
-           ? "Something went wrong!"
-           : loading
-           ? "loading"
-           : data?.map((item) => <Card item={item} key={item.id} />)}
-       </div>
-     </div>
+
     </div>
   )
 }
