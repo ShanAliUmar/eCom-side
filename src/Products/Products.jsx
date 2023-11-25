55ebd9da4de794d294d934b4f7e86e636c26d68e
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-25 22:51:30 +0500

    sdk

--- src/Products/Products.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import "../Products/Produets.scss"
-const Products = () => {
-  return (
-    <div className='products'>Products</div>
-  )
-}
-
-export default Products
---
+++ src/pages/Products/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Produets.scss"
+const Products = () => {
+  return (
+    <div className='products'>
+      <div className="left"></div>
+      <div className="right"></div>
+    </div>
+  )
+}
+
+export default Products
