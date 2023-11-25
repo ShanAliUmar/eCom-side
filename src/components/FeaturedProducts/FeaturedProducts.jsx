a99b384f1943f925b5012592b62f167e1b3f0b75
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-25 17:28:26 +0500

    hsjk

--- src/components/FeaturedProducts/FeaturedProducts.jsx
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -6,8 +6,8 @@ const FeaturedProducts = ({ type }) => {
   let dataImg = [
     {
       id: 1,
-      img1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrVN9H11wCam0PY3Wp44gEjVOWihP2BNyltg&usqp=CAU",
-      img2: "",
+      img1: "https://i0.wp.com/pixahive.com/wp-content/uploads/2021/03/Stylish-boy-posing-in-front-of-car-391363-pixahive.jpg?fit=1707%2C2560&ssl=1",
+      img2: "https://i.pinimg.com/736x/25/f6/e0/25f6e09aa4e3a2b53b964b20e8114174.jpg",
       title: "Long Sleeve Graphic T-shirt",
       isNew: true,
       oldPrice: 19,
@@ -15,8 +15,8 @@ const FeaturedProducts = ({ type }) => {
     },
     {
       id: 2,
-      img1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxGAgkfAXaaaCWR6GVBY4NK6v_xP4DVSeVvm9gHX4T_x7nmlO7B414VgVQA5Dlj7uKgf8&usqp=CAU",
-      img2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxGAgkfAXaaaCWR6GVBY4NK6v_xP4DVSeVvm9gHX4T_x7nmlO7B414VgVQA5Dlj7uKgf8&usqp=CAU",
+      img1: "https://i.pinimg.com/474x/b4/c6/be/b4c6be8403d714b40153aff00398704f.jpg",
+      img2: "https://i.pinimg.com/236x/9b/21/97/9b21977a67533d6a9357f84f3d7b85d3.jpg",
       title: "Coat",
       isNew: true,
       oldPrice: 19,
@@ -24,16 +24,16 @@ const FeaturedProducts = ({ type }) => {
     },
     {
       id: 3,
-      img2: "https://img.freepik.com/premium-photo/captivating-vibes-mexican-beauty-shines-black-short-within-photographic-haven_983420-46056.jpg?size=338&ext=jpg&ga=GA1.1.1880011253.1699488000&semt=ais",
-      img1: "https://img.freepik.com/premium-photo/muscular-handsome-man-black-tshirt-realistic-t-shirt-mockup_911620-9461.jpg",
+      img1: "https://e1.pxfuel.com/desktop-wallpaper/202/582/desktop-wallpaper-indian-boy-pic-indian-boy.jpg",
+      img2: "https://images.unsplash.com/photo-1598096969068-7f52cac10c83?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8aW5kaWFuJTIwYm95fGVufDB8fDB8fHww",
       title: "Skirt",
       oldPrice: 19,
       price: 12
     },
     {
       id: 4,
-      img1: "https://images.unsplash.com/photo-1606810745463-dfdf9626320b?w=420&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8VjZpV3V1OHJKdlV8fGVufDB8fHx8fA%3D%3D",
-      img2: "https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?cs=srgb&dl=pexels-tu%E1%BA%A5n-ki%E1%BB%87t-jr-1391498.jpg&fm=jpg",
+      img1: "https://images.pexels.com/photos/1391498/pexels-photo-1391498.jpeg?cs=srgb&dl=pexels-tu%E1%BA%A5n-ki%E1%BB%87t-jr-1391498.jpg&fm=jpg",
+      img2: "https://images.unsplash.com/photo-1606810745463-dfdf9626320b?w=420&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8VjZpV3V1OHJKdlV8fGVufDB8fHx8fA%3D%3D",
       title: "Hat",
       oldPrice: 19,
       price: 12
