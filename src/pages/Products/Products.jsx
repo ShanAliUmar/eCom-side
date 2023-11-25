eb6dc384c74c3b2f495a000532f8ad13defb5a06
ShanAliUmar <shanumar.dev@gmail.com>
2023-11-25 22:55:43 +0500

    sdk

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -3,7 +3,18 @@ import "./Produets.scss"
 const Products = () => {
   return (
     <div className='products'>
-      <div className="left"></div>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Products Categories</h2>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+        </div>
+
+      </div>
       <div className="right"></div>
     </div>
   )
